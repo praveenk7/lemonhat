13bf18a777a954978fcba8e98ef227b5617db24c
praveen <praveen.k7@gmail.com>
2017-11-08 15:15:55 +0530

    angular setup

---
+++ app/app.module.ts
@@ -0,0 +1,23 @@
+﻿import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+//components
+ import { AppComponent }  from './app.component';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    declarations: [
+        AppComponent      
+    ],
+    providers: [      
+    ],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
